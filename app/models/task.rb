18855b1ab36bec0dc2acb2bb645f99cc3831b29d
d7shah <dhrutishah10@gmail.com>
2015-05-07 11:44:25 +0300

    added validations to our task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,14 @@
 class Task < ActiveRecord::Base
 	belongs_to :user
+	validates :user_id, presence: true
+	validates :content, presence:true
 
 	auto_html_for :content do
 		html_escape
 		image
 		youtube(:width => "100%", :heigh => 250, :autoplay => false)
 		link :target => "_blank", :rel => "notfollow"
+		simple_format
 
 	end
 
